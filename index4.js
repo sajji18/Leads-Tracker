2a37c0348d5a2517203e20dba177593f42620079
Sajal Chauhan <sajalchauhan2004@gmail.com>
2023-06-29 00:48:52 +0530

    Add existing file

--- index4.js
+++ index4.js
@@ -4,23 +4,31 @@
 
 const inputBtn = document.getElementById("input-btn")
 const inpulEl = document.getElementById("input-el")
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const unEl = document.getElementById("ul-el")
 let myLeads = []
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inpulEl.value)
-    renderLeads()
-    inpulEl.value = ""
-})
 
-function renderLeads() {
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+// Array with object as element
+// const tabs = [
+//     {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+// ]
+
+function render(leads) {
     let listItems = ""
-    for(let i=0;i<myLeads.length;i++){
+    for(let i=0;i<leads.length;i++){
         // listItems += "<li><a target='_blank' href = '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
         <li>
-        <a target='_blank' href = '${myLeads[i]}'>
-        ${myLeads[i]}
+        <a target='_blank' href = '${leads[i]}'>
+        ${leads[i]}
         </a>
         </li>`
 
@@ -31,3 +39,34 @@ function renderLeads() {
     }
     unEl.innerHTML = listItems
 }
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inpulEl.value)
+    inpulEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads)
+
+})
+
+
+tabBtn.addEventListener("click", function(){
+    // window.open(tabs[0].url, '_blank')
+    // Grab URL of current tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+      })
+})
+
+
+deleteBtn.addEventListener("dblclick", function(){
+    console.log("double clicked")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
---
+++ test.js
@@ -0,0 +1,29 @@
+// let myLeads = `["www.awesomeleads.com"]`
+
+// // makes string an array
+// myLeads = JSON.parse(myLeads)
+
+// myLeads.push("www.example.com")
+
+// // makes array a string
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof(myLeads))
+
+// const welcomeEl = document.getElementById("welcome-el")
+
+// function greetUser(greeting, name){
+//     welcomeEl.textContent = `${greeting}, ${name} ✋`
+// }
+
+// greetUser("Howdy", "James")
+// let array = [1, 2, 3]
+
+// function getFirst(arr){
+//     return arr[0]
+// }
+
+// console.log(getFirst(array))
+
+// localStorage.setItem("leads", "xyz")
+// let value = localStorage.getItem("leads")
+// console.log(value)
